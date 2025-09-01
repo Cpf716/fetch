d30f7ab682b5a0f1868aa936b876e3cb56f3cd64
Corey Ferguson <cpf716@gmail.com>
2025-09-01 14:14:50 -0400

    Initial Commit

---
+++ fetch/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  fetch
+//
+//  Created by Corey Ferguson on 9/1/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
