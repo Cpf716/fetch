88d193eb65632f950202a925d80c54395779a137
Corey Ferguson <cpf716@gmail.com>
2025-09-06 17:18:03 -0400

    task-9

--- fetch/src/main.cpp
+++ fetch/src/main.cpp
@@ -8,20 +8,27 @@
 #include "fetch.h"
 
 using namespace fetch;
+using namespace mysocket;
 using namespace std;
 
 map<string, string> headers = {{ "content-type", "application/json" }};
 
 int main(int argc, const char * argv[]) {
-    string              url = "http://localhost:8081/greeting";
-    string              method = "POST";
-    string              body = "{ \"firstName\": \"Corey\" }";
+    // auto server = new tcp_server(8082);
+
+    string url = "http://localhost:8081/greeting";
+    // string url = "http://localhost:8082/greeting";
+
+    string method = "POST";
+    string body = "{ \"firstName\": \"Corey\" }";
 
     try {
         auto response = request(headers, url, method, body);
 
         cout << response.text() << endl;
     } catch (fetch::error& e) {
+        // server->close();
+
         if (e.text().length())
             throw fetch::error(e.status(), e.text(), e.text(), e.headers());
 
